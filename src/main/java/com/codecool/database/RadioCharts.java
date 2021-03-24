fea67d324e7b3c267dc7ac1229e215d5320b8da7
David Kajdarov <kajdarov.d@gmail.com>
2021-03-24 17:35:40 +0100

    Finished the project

--- src/main/java/com/codecool/database/RadioCharts.java
+++ src/main/java/com/codecool/database/RadioCharts.java
@@ -1,6 +1,48 @@
 package com.codecool.database;
 
 
+import java.sql.*;
+
 public class RadioCharts {
-    //TODO: implement this class
+
+    private final String DB_URL;
+    private final String DB_USER;
+    private final String DB_PASSWORD;
+
+    public RadioCharts(String DB_URL, String DB_USER, String DB_PASSOWORD) {
+        this.DB_URL = DB_URL;
+        this.DB_USER = DB_USER;
+        this.DB_PASSWORD = DB_PASSOWORD;
+    }
+
+    public String getMostPlayedSong() {
+        String query = "SELECT song, MAX(times_aired) " +
+                "FROM music_broadcast " +
+                "GROUP BY song " +
+                "ORDER BY SUM (times_aired) DESC";
+
+        return getResult(query, "song");
+    }
+
+    public String getMostActiveArtist() {
+        String query = "SELECT artist, song " +
+                "FROM music_broadcast " +
+                "GROUP BY artist " +
+                "ORDER BY COUNT(song) DESC";
+
+        return getResult(query, "artist");
+    }
+
+    private String getResult(String query, String column) {
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(query);
+            if (rs.next()) {
+                return (rs.getString(column));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
 }
